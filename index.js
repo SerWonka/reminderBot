c3bbd647cbd9c6dde5d5fe601a6f6e06a3c17279
serwonka <seiran.arakelyan@cengage.com>
2020-03-11 10:35:15 +0200

    editet time message

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ bot.start((ctx) => {
     ctx.reply('Здарово-ствуй я вроде как бот, и вроде как буду оповещять о том что у вас тут душно (а тут реально душо, ага)')
     const cron = require('node-cron');
 
-    cron.schedule('18 12 * * *', () => {
+    cron.schedule('45 11 * * *', () => {
         ctx.reply('Надо проветрить!!!');
     });
 
-    cron.schedule('17 12 * * *', () => {
+    cron.schedule('45 14 * * *', () => {
         ctx.reply('Надо проветрить!!!');
     });
 
-    cron.schedule('16 12 * * *', () => {
+    cron.schedule('45 16 * * *', () => {
         ctx.reply('Надо проветрить!!!');
     });
 })
